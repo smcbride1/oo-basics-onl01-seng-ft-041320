b002ecfaa2d7fce2b79800f0cf7c8b8ae3943d23
Stephen McBride <stevemcbride3@gmail.com>
2020-04-21 22:13:29 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,34 +1,8 @@
 class Book
+  attr_reader :title
+  attr_accessor :page_count. :author, :genre
   def initialize(title)
     @title = title
-  end
-
-  def title
-    @title
-  end
-
-  def author
-    @author
-  end
-
-  def author=(author)
-    @author = author
-  end
-
-  def page_count
-    @page_count
-  end
-
-  def page_count=(page_count)
-    @page_count = page_count
-  end
-
-  def genre
-    @genre
-  end
-
-  def genre=(genre)
-    @genre = genre
   end
 
   def turn_page
